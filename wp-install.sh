59cbbb5ff5c4d6fce3fe934d9d9e4c672c3ce2e1
Bajpan Gosh <bajpangosh@gmail.com>
2019-04-20 11:51:50 +0530

    close color

--- wp-install.sh
+++ wp-install.sh
@@ -6,14 +6,17 @@ tput setaf 2; echo "Username (eg. database name)?"
 read USERNAME
 tput setaf 2; echo "Updating OS................."
 sleep 2;
+tput sgr0
 sudo apt-get update
 
 tput setaf 2; echo "Installing Nginx"
 sleep 2;
+tput sgr0
 sudo apt-get install nginx zip unzip pwgen -y
 
 tput setaf 2; echo "Sit back and relax :) ......"
 sleep 2;
+tput sgr0
 cd /etc/nginx/sites-available/
 
 sudo wget -qO "$DOMAIN" https://raw.githubusercontent.com/bajpangosh/High-Traffic-wordpress-server-configuration/master/sites-available/example.com.conf
@@ -26,6 +29,7 @@ sudo wget -q https://raw.githubusercontent.com/bajpangosh/High-Traffic-wordpress
 sudo wget -q https://raw.githubusercontent.com/bajpangosh/High-Traffic-wordpress-server-configuration/master/kloudboy/php_fastcgi.conf
 tput setaf 2; echo "Setting up Cloudflare FULL SSL"
 sleep 2;
+tput sgr0
 sudo mkdir /etc/nginx/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
@@ -38,6 +42,7 @@ cd ~
 
 tput setaf 2; echo "Downloading Latest Wordpress...."
 sleep 2;
+tput sgr0
 sudo wget -q wordpress.org/latest.zip
 sudo unzip latest.zip
 sudo mv wordpress/* /var/www/"$DOMAIN"/public/
@@ -45,17 +50,20 @@ sudo rm -rf wordpress latest.zip
 
 tput setaf 2; echo "Nginx server installation completed.."
 sleep 2;
+tput sgr0
 cd ~
 sudo chown www-data:www-data -R /var/www/"$DOMAIN"/public
 sudo systemctl restart nginx.service
 
 tput setaf 2; echo "let's install php 7.2 and modules"
 sleep 2;
+tput sgr0
 sudo apt install php7.2 php7.2-fpm -y
 sudo apt-get -y install php7.2-intl php7.2-curl php7.2-gd php7.2-imap php7.2-readline php7.2-common php7.2-recode php7.2-mysql php7.2-cli php7.2-curl php7.2-mbstring php7.2-bcmath php7.2-mysql php7.2-opcache php7.2-zip php7.2-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.2-soap
 
 tput setaf 2; echo "Some php.ini Tweaks"
 sleep 2;
+tput sgr0
 sudo sed -i "s/post_max_size = .*/post_max_size = 2000M/" /etc/php/7.2/fpm/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 3000M/" /etc/php/7.2/fpm/php.ini
 sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.2/fpm/php.ini
@@ -65,6 +73,7 @@ sudo systemctl restart php7.2-fpm.service
 
 tput setaf 2; echo "Instaling MariaDB"
 sleep 2;
+tput sgr0
 sudo apt install mariadb-server mariadb-client php7.2-mysql -y
 sudo systemctl restart php7.2-fpm.service
 sudo mysql_secure_installation
