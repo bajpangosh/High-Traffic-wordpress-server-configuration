0dbc1893c423af8ddbcd4bc7e116b1bd3600bdfe
Bajpan Gosh <bajpangosh@gmail.com>
2020-07-10 18:19:23 +0530

    PHP 7.4 Update & Ubunthu 20 Support
    PHP 7.4 Update Ubunthu 20 Support

--- wp-install.sh
+++ wp-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GET ALL USER INPUT
-tput setaf 2; echo "Domain Name (eg. example.com)?"
+tput setaf 2; echo "Domain Name (eg. yoursitename.com)?"
 read DOMAIN
 tput setaf 2; echo "Username (eg. database name)?"
 read USERNAME
@@ -42,7 +42,12 @@ sudo wget -qO nginx.conf https://raw.githubusercontent.com/bajpangosh/High-Traff
 sudo mkdir -p /var/www/"$DOMAIN"/public
 cd /var/www/"$DOMAIN/public"
 cd ~
-
+tput setaf 2; echo "Installing WP-CLI...."
+sleep 2;
+tput sgr0
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
 tput setaf 2; echo "Downloading Latest Wordpress...."
 sleep 2;
 tput sgr0
@@ -50,38 +55,34 @@ sudo wget -q wordpress.org/latest.zip
 sudo unzip latest.zip
 sudo mv wordpress/* /var/www/"$DOMAIN"/public/
 sudo rm -rf wordpress latest.zip
-
 tput setaf 2; echo "Nginx server installation completed.."
 sleep 2;
 tput sgr0
 cd ~
 sudo chown www-data:www-data -R /var/www/"$DOMAIN"/public
 sudo systemctl restart nginx.service
-
-tput setaf 2; echo "let's install php 7.3 and modules"
+tput setaf 2; echo "let's install php 7.4 and modules"
 sleep 2;
 tput sgr0
-sudo apt install php7.3 php7.3-fpm -y
-sudo apt-get -y install php7.3-intl php7.3-curl php7.3-gd php7.3-imap php7.3-readline php7.3-common php7.3-recode php7.3-mysql php7.3-cli php7.3-curl php7.3-mbstring php7.3-bcmath php7.3-mysql php7.3-opcache php7.3-zip php7.3-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.3-soap
+sudo apt install php7.4 php7.4-fpm -y
+sudo apt-get -y install php7.4-intl php7.4-curl php7.4-gd php7.4-imap php7.4-readline php7.4-common php7.4-recode php7.4-mysql php7.4-cli php7.4-curl php7.4-mbstring php7.4-bcmath php7.4-mysql php7.4-opcache php7.4-zip php7.4-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.4-soap
 tput setaf 2; echo "Some php.ini Tweaks"
 sleep 2;
 tput sgr0
-sudo sed -i "s/post_max_size = .*/post_max_size = 2000M/" /etc/php/7.3/fpm/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 3000M/" /etc/php/7.3/fpm/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.3/fpm/php.ini
-sudo sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.3/fpm/php.ini
-sudo sed -i "s/;max_input_vars = .*/max_input_vars = 5000/" /etc/php/7.3/fpm/php.ini
-sudo sed -i "s/max_input_time = .*/max_input_time = 1000/" /etc/php/7.3/fpm/php.ini
-sudo systemctl restart php7.3-fpm.service
-
+sudo sed -i "s/post_max_size = .*/post_max_size = 2000M/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 3000M/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/;max_input_vars = .*/max_input_vars = 5000/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/max_input_time = .*/max_input_time = 1000/" /etc/php/7.4/fpm/php.ini
+sudo systemctl restart php7.4-fpm.service
 tput setaf 2; echo "Instaling MariaDB"
 sleep 2;
 tput sgr0
-sudo apt install mariadb-server mariadb-client php7.3-mysql -y
-sudo systemctl restart php7.3-fpm.service
+sudo apt install mariadb-server mariadb-client php7.4-mysql -y
+sudo systemctl restart php7.4-fpm.service
 sudo mysql_secure_installation
 PASS=`pwgen -s 14 1`
-
 sudo mysql -uroot <<MYSQL_SCRIPT
 CREATE DATABASE $USERNAME;
 CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASS';
