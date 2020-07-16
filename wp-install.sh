e3f28afd3ca6eb0a66b727e27424ed95054cf820
Bajpan Gosh <bajpangosh@gmail.com>
2020-07-16 21:03:46 +0530

    Update wp-install.sh
    php modules updated.

--- wp-install.sh
+++ wp-install.sh
@@ -65,7 +65,7 @@ tput setaf 2; echo "let's install php 7.4 and modules"
 sleep 2;
 tput sgr0
 sudo apt install php7.4 php7.4-fpm -y
-sudo apt-get -y install php7.4-intl php7.4-curl php7.4-gd php7.4-imap php7.4-readline php7.4-common php7.4-recode php7.4-mysql php7.4-cli php7.4-curl php7.4-mbstring php7.4-bcmath php7.4-mysql php7.4-opcache php7.4-zip php7.4-xml php-memcached php-imagick php-memcache memcached graphviz php-pear php-xdebug php-msgpack  php7.4-soap
+sudo apt install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl -y
 tput setaf 2; echo "Some php.ini Tweaks"
 sleep 2;
 tput sgr0
