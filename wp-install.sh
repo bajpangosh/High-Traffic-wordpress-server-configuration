106d1b2218525f874d3fc5da2ed6159746ca46cc
Bajpan Gosh <bajpangosh@gmail.com>
2020-08-25 19:30:42 +0530

    Update wp-install.sh

--- wp-install.sh
+++ wp-install.sh
@@ -69,7 +69,7 @@ sudo apt install php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl
 tput setaf 2; echo "Some php.ini Tweaks"
 sleep 2;
 tput sgr0
-sudo sed -i "s/post_max_size = .*/post_max_size = 2000M/" /etc/php/7.4/fpm/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.4/fpm/php.ini
 sudo sed -i "s/memory_limit = .*/memory_limit = 3000M/" /etc/php/7.4/fpm/php.ini
 sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.4/fpm/php.ini
 sudo sed -i "s/max_execution_time = .*/max_execution_time = 18000/" /etc/php/7.4/fpm/php.ini
